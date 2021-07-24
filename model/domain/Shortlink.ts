6cd7f99e90269e40ba1b2490b824533c9d3b5721
Mellina Y <yonashiro.mellina@gmail.com>
2021-07-24 08:54:19 -0300

    setup: create model classes (domain and services)

--- model/domain/Shortlink.ts
+++ model/domain/Shortlink.ts
@@ -1,3 +1,12 @@
 export class Shortlink {
+  originalUrl: string = "";
+  shortUrl: string = "";
 
+  constructor(data: Shortlink) {
+    Object.assign(this, data);
+  }
+
+  copy(data: Partial<Shortlink>) {
+    return Object.assign(this, data);
+  }
 }
--- model/services/ShortlinkService.ts
+++ model/services/ShortlinkService.ts
@@ -1,3 +1,6 @@
 export class ShortlinkService {
-
+  async getShortlink(url: string) {
+    const shortlink = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`);
+    return shortlink;
+  }
 }
