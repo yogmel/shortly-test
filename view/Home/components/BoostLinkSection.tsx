fcee243d9105e93e65dee458da567848459cade5
Mellina Y <yonashiro.mellina@gmail.com>
2021-07-24 22:47:11 -0300

    style: BoostLink Section

--- view/Home/components/BoostLinkSection.tsx
+++ view/Home/components/BoostLinkSection.tsx
@@ -1,10 +1,14 @@
+import React from "react";
+import { Button, ButtonType } from "view/shared";
+import boostLinkStyles from "./../styles/BoostLinkSection.module.scss";
+
 export default function BoostLinkSection() {
   return (
-    <section>
-      <div>
-        <h2>Boost your links today</h2>
-        <button>Get Started</button>
-      </div>
+    <section className={boostLinkStyles.section}>
+      <h2 className="inverted">Boost your links today</h2>
+      <Button type={ButtonType.Primary} className={boostLinkStyles.button}>
+        Get Started
+      </Button>
     </section>
   );
 }
